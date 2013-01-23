983780abdafad10796172022a690e406459bdd64
Joseph Utecht <thenmal@gmail.com>
2013-01-23 01:00:34 -0600

    multi-broadcast working

--- server/server.py
+++ server/server.py
@@ -2,21 +2,23 @@ from tornado import websocket
 import tornado
 import tornado.ioloop
 
+listeners = []
+
 class EchoWebSocket(websocket.WebSocketHandler):
     def open(self):
         print "WebSocket opened"
+        listeners.append(self)
 
     def on_message(self, message):
-        self.write_message(u"You said: " + message)
+        for l in listeners:
+            l.write_message(u"You said: " + message)
 
     def on_close(self):
         print "WebSocket closed"
+        listeners.remove(self)
 
 if __name__ == "__main__":
     application = tornado.web.Application([
-        (r"/", EchoWebSocket),
-    ])
+        (r"/", EchoWebSocket),])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
-    
-        
